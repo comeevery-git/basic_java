9ed835c4292222db89f85e5b95dcc74d6973a636
suhee <comeevery@gmail.com>
2024-01-24 21:37:19 +0900

    test: thread test disabled

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -10,61 +10,60 @@ import static java.lang.Thread.getAllStackTraces;
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        // SpringApplication.run(Application.class, args);
+         SpringApplication.run(Application.class, args);
 
 
-        Thread thread1 = Thread.ofPlatform()
-                .name("thread1")
-                .unstarted(() -> {
-                    System.out.println("thread1");
-                    int res = 0;
-                    for(int i = 0; i < 100; i++){
-                        res += i;
-                    }
-                });
-        System.out.println("1# isVirtual:" + thread1.isVirtual());
-
-        Thread thread2 = Thread.ofVirtual()
-                .name("thread2")
-                .unstarted(() -> {
-                    System.out.println("thread2");
-                    getAllStackTraces().forEach((thread, stackTraceElements) -> {
-                        int res = 0;
-                        for(int i = 0; i < 100; i++){
-                            res += i;
-                        }
-                    });
-                });
-        System.out.println("2# isVirtual:" + thread2.isVirtual());
-
-
-        long normalStart = System.currentTimeMillis();
-        for(int i = 0; i < 100000; i++){
-            Thread th = Thread.ofPlatform().unstarted(thread1);
-            th.start();
-            try {
-                th.join();
-            } catch (Exception e){
-                e.printStackTrace();
-            }
-        }
-        long normalEnd = System.currentTimeMillis();
-
-        long vStart = System.currentTimeMillis();
-        for(int i = 0; i < 100000; i++){
-            Thread th = Thread.ofVirtual().unstarted(thread2);
-            th.start();
-            try {
-                th.join();
-            } catch (Exception e){
-                e.printStackTrace();
-            }
-        }
-        long vEnd = System.currentTimeMillis();
-
-        System.out.println("Normal Thread Time : " + (normalEnd - normalStart) / 1000);
-        System.out.println("Virtual Thread Time : " + (vEnd - vStart) / 1000);
-
+//        Thread thread1 = Thread.ofPlatform()
+//                .name("thread1")
+//                .unstarted(() -> {
+//                    System.out.println("thread1");
+//                    int res = 0;
+//                    for(int i = 0; i < 100; i++){
+//                        res += i;
+//                    }
+//                });
+//        System.out.println("1# isVirtual:" + thread1.isVirtual());
+//
+//        Thread thread2 = Thread.ofVirtual()
+//                .name("thread2")
+//                .unstarted(() -> {
+//                    System.out.println("thread2");
+//                    getAllStackTraces().forEach((thread, stackTraceElements) -> {
+//                        int res = 0;
+//                        for(int i = 0; i < 100; i++){
+//                            res += i;
+//                        }
+//                    });
+//                });
+//        System.out.println("2# isVirtual:" + thread2.isVirtual());
+//
+//
+//        long normalStart = System.currentTimeMillis();
+//        for(int i = 0; i < 100000; i++){
+//            Thread th = Thread.ofPlatform().unstarted(thread1);
+//            th.start();
+//            try {
+//                th.join();
+//            } catch (Exception e){
+//                e.printStackTrace();
+//            }
+//        }
+//        long normalEnd = System.currentTimeMillis();
+//
+//        long vStart = System.currentTimeMillis();
+//        for(int i = 0; i < 100000; i++){
+//            Thread th = Thread.ofVirtual().unstarted(thread2);
+//            th.start();
+//            try {
+//                th.join();
+//            } catch (Exception e){
+//                e.printStackTrace();
+//            }
+//        }
+//        long vEnd = System.currentTimeMillis();
+//
+//        System.out.println("Normal Thread Time : " + (normalEnd - normalStart) / 1000);
+//        System.out.println("Virtual Thread Time : " + (vEnd - vStart) / 1000);
 
 
     }
