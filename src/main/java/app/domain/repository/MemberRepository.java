7b7cc011659bbb4d4b1e22e53ccaa8793494bfe5
suhee <comeevery@gmail.com>
2024-01-24 22:25:12 +0900

    feat: Modify MemberRepository, Add customRepo
    - Querydsl 사용하는 Repository 추가
    - 기본 JpaRepository 구현체와 Querydsl 사용하는 CustomRepository 같이 사용할 수 있도록 수정

--- src/main/java/app/domain/repository/MemberRepository.java
+++ src/main/java/app/domain/repository/MemberRepository.java
@@ -1,8 +1,9 @@
 package app.domain.repository;
 
 import app.domain.model.entity.member.Member;
+import app.domain.repository.custom.CustomMemberRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>, CustomMemberRepository {
 
 }
--- src/main/java/app/infrastructure/persistence/MemberRepositoryImpl.java
+++ src/main/java/app/infrastructure/persistence/MemberRepositoryImpl.java
@@ -1,8 +1,24 @@
-//package app.infrastructure.persistence;
-//
-//import app.domain.repository.MemberRepository;
-//
-//public class MemberRepositoryImpl implements MemberRepository {
-//
-//
-//}
+package app.infrastructure.persistence;
+
+import app.domain.model.entity.member.Member;
+import app.domain.model.entity.member.QMember;
+import app.domain.repository.custom.CustomMemberRepository;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class MemberRepositoryImpl implements CustomMemberRepository {
+    private final JPAQueryFactory query;
+    private QMember qMember = QMember.member;
+
+    @Override
+    public Member findByEmail(String email) {
+        return query
+                .selectFrom(qMember)
+                .where(
+                        qMember.email.eq(email)
+                )
+                .fetchOne();
+    }
+
+}
