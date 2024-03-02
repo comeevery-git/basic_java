22e4c9fa6e3e4c4ba9d0d0ec124f66dc53e7d1c4
suhee <comeevery@gmail.com>
2024-03-03 08:09:31 +0900

    feat: Add CommonResponseCode

--- src/main/java/app/presentation/MemberController.java
+++ src/main/java/app/presentation/MemberController.java
@@ -1,20 +1,26 @@
 package app.presentation;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import app.application.dto.member.CreateMemberDto;
 import app.application.vo.member.CreateMemberVo;
 import app.application.vo.member.MemberVo;
 import app.domain.model.common.BaseResponse;
 import app.domain.service.MemberService;
+import app.infrastructure.annotation.CommonResponseCode;
 import app.infrastructure.exception.CustomException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Member", description = "회원 정보 관련 API")
 @Slf4j
@@ -26,11 +32,8 @@ public class MemberController {
 
     @GetMapping("/{memberId}")
     @Operation(summary = "회원 정보 조회", description = "회원 정보를 조회합니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "성공"
-                    , content = @Content(schema = @Schema(implementation = MemberVo.class))),
-            @ApiResponse(responseCode = "500", description = "시스템 오류가 발생했습니다."),
-    })
+    @CommonResponseCode
+    @ApiResponse(responseCode = "200", description = "성공", content = @Content(schema = @Schema(implementation = MemberVo.class)))
     public BaseResponse<MemberVo> getMember(@PathVariable(name = "memberId") final Long memberId) {
         try {
             MemberVo memberVo = memberService.getMember(memberId);
@@ -44,11 +47,8 @@ public class MemberController {
 
     @PostMapping
     @Operation(summary = "회원 정보 등록", description = "회원 정보를 등록합니다.")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "성공"
-                    , content = @Content(schema = @Schema(implementation = CreateMemberVo.class))),
-            @ApiResponse(responseCode = "500", description = "시스템 오류가 발생했습니다."),
-    })
+    @CommonResponseCode
+    @ApiResponse(responseCode = "200", description = "성공", content = @Content(schema = @Schema(implementation = CreateMemberVo.class)))
     public BaseResponse<CreateMemberVo> createMember(@RequestBody final CreateMemberDto createMemberDto) {
         log.info("createMemberDto: {}", createMemberDto);
         try {
